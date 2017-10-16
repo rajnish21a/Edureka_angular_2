737dcde334cf6d9c5d2c5b825dc0409cf2e12540
imrajnish <rajnish.rai@learningmate.com>
2017-10-16 17:58:55 +0530

    Initial Commit and Root Component

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector:'my-app',
+    template:`
+        <h1>Edureka Angular 2.0<h1>
+    `
+})
+
+
+export class RootComponent {
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+import { RootComponent} from './app.component';
+
+@NgModule({
+    imports:[
+        BrowserModule,
+        FormsModule,
+        CommonModule
+    ],
+    exports:[],
+    declarations:[RootComponent],
+    providers:[],
+    bootstrap:[RootComponent]
+})
+
+export class AppModule {
+
+}
