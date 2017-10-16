3690bc069e333d144b124a4d40463529e98a4485
imrajnish <rajnish.rai@learningmate.com>
2017-10-16 18:41:28 +0530

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,15 @@ import { Component } from '@angular/core';
 @Component({
     selector:'my-app',
     template:`
-        <h1>Edureka Angular 2.0<h1>
+        <h1>Edureka Angular 2.0</h1>
+        {{test}}
+        <br>
+        <br>
+        <input type="text" placeholder="Name" [(ngModel)]="test">
     `
 })
 
 
 export class RootComponent {
-
+    test: string = "My name is Rajnish";
 }
